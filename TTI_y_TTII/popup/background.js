aa280e743a0d0f85c2c670cc4e1ae2f96197cb29
Armando Castro <darkar.98@hotmail.com>
2022-03-14 18:22:07 -0600

    Versión muy antigua, deprecated
    Pero gracias a esta no morimos xD

--- TTI_y_TTII/popup/background.js
+++
@@ -1,9 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(activeTab){
-    var newURL = "/popup/main.html";
-    chrome.tabs.create({ url: newURL });
-  });
-  chrome.tabs.getAllInWindow(null,function(tabs){
-    for (let i = 0;i < tabs.length;i++){
-      console.log(tabs[i]);
-    }
-  })
