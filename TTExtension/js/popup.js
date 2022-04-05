930bdda427c2192c175d89e3c25f4d224189a257
Armando Castro <darkar.98@hotmail.com>
2022-04-04 19:08:15 -0500

    Update 04/04/2022
    Lol

--- TTExtension/js/popup.js
+++ TTExtension/js/popup.js
@@ -1,46 +1,44 @@
-window.onload = apply();
+var flagVozp = true;
+var flagTTSp = true;
+TTsinicio = document.getElementById('TextToSpeech');
+comandoInicio = document.getElementById('NavegacionPorVoz');
+textTTsinicio = outer(TTsinicio);
+textcomandoInicio = outer(comandoInicio);
+window.onload = apply(0);
 
-function apply() {
-    var flagVoice, flagTTS;
+function apply(lol) {
     var idioma = document.getElementById("idiomas");
     var selIdioma = idioma.options[idioma.selectedIndex].text;
     var voz = document.getElementById("voces");
     var selVoz = voz.options[voz.selectedIndex].text;
-    var textToSpeech = document.getElementById("TextToSpeech");
-    var navegacionPorVoz =document.getElementById("NavegacionPorVoz")
     var val1 = outer(voz);
-    var val2 = outer(textToSpeech);
-    var val3 = outer(navegacionPorVoz);
-    var val4 = selIdioma;
-    var val5 = selVoz;
-    if(val2.includes('checked')) {
-        flagTTS = true;
-    } else {
-        flagTTS = false;
+    var val2 = selIdioma;
+    var val3 = selVoz;
+    if(lol == 1) {
+        console.log(flagTTSp);
+        console.log(flagVozp);
+        if(flagTTSp == true) {
+            document.getElementById("ttsSwitch").innerHTML = '<input type="checkbox" id="TextToSpeech"><span class="slider round"></span>';
+        } else {
+            document.getElementById("ttsSwitch").innerHTML = '<input type="checkbox" id="TextToSpeech" checked><span class="slider round"></span>';
+        }
+        if(flagVozp == true) {
+            document.getElementById("vozSwitch").innerHTML = '<input type="checkbox" id="NavegacionPorVoz"><span class="slider round"></span>';
+        } else {
+            document.getElementById("vozSwitch").innerHTML = '<input type="checkbox" id="NavegacionPorVoz" checked><span class="slider round"></span>';
+        }
     }
-    if (val3.includes('checked')) {
-        flagVoice = true;
-    } else {
-        flagVoice = false;
-    }
-    let msg = {data1: val1,data2: val2,data3: val3,data4: val4,data5: val5, banderaTTS:flagTTS, banderaVoice: flagVoice}
+    let msg = {data1: val1,data2: val2,data3: val3, banderaTTS:flagTTSp, banderaVoice: flagVozp}
     chrome.storage.sync.set({'container': msg},function() {
         console.log("Container Enviado");
+        console.log(msg);
     });
 }
 
 function voiceTest() {
-    var flagTTS;
-    var textToSpeech = document.getElementById("TextToSpeech");
-    var funcionalidad = outer(textToSpeech);
-    if(funcionalidad.includes('checked')) {
-        flagTTS = true;
-    } else {
-        flagTTS = false;
-    }
     var voz = document.getElementById("voces");
     var selVoz = voz.options[voz.selectedIndex].text;
-    let mesange = {dato1:selVoz, bandera: flagTTS};
+    let mesange = {dato1:selVoz, bandera: flagTTSp};
     chrome.storage.sync.set({'testVoz': mesange},function() {
         console.log("TestVoz Enviado");
     });
@@ -63,9 +61,27 @@ function outer(element){
 document.getElementById('idioma').addEventListener();
 */
 
+document.getElementById("TextToSpeech").addEventListener("click", function() {
+    if(textTTsinicio.includes('checked')) {
+        flagTTSp = false;
+    } else {
+        flagTTSp = true;
+    }
+    console.log(flagTTSp);
+});
+
+document.getElementById("NavegacionPorVoz").addEventListener("click", function() {
+    if(textcomandoInicio.includes('checked')) {
+        flagVozp = false;
+    } else {
+        flagVozp = true;
+    }
+    console.log(flagVozp);
+});
+
 document.getElementById("aplicarCambios").addEventListener("click", function() {
     console.log('Funcion Aplicar Cambios');
-    apply();
+    apply(1);
 });
 
 document.getElementById("pruebaVoz").addEventListener("click", function() {
